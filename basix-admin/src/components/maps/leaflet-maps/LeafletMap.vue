cb8ce306ca80187d448c51c7e78693eb717f1291
nlho@yahoo.com <nlho@yahoo.com>
2019-09-22 14:51:40 +0800

    Google map with home location

--- basix-admin/src/components/maps/leaflet-maps/LeafletMap.vue
+++ basix-admin/src/components/maps/leaflet-maps/LeafletMap.vue
@@ -13,7 +13,16 @@
 //    L.Icon.Default.imagePath = 'assets/vendor/leaflet' TODO: make it work with webpack
       L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.0.3/dist/images'
 
-      var map = L.map(this.$el).setView([ 1.3805551,103.8422801 ], 13)
+      var home_location = [ 1.3805551,103.8422801 ];
+      var map = L.map(this.$el).setView(home_location, 13)
+
+      map.on('locationfound', function onLocationFound(e) {
+                                var radius = e.accuracy;
+                                L.marker(e.latlng).addTo(map)
+                                  .bindPopup("You are within " + radius + " meters from this point").openPopup();
+                                L.circle(e.latlng, radius).addTo(map);
+                              }
+            );
 
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
